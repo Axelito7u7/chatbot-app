172218045bd4b3ed360a1b0dcd0d34690f98045b
Axelito7u7 <galvanflores102@gmail.com>
2024-09-04 17:59:02 -0600

    conexionFirebase

---
+++ src/firebase_config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAe5xaVV0uyTfoKLcQcOw-jYYaA12i8aug",
+    authDomain: "chatbot-d9174.firebaseapp.com",
+    projectId: "chatbot-d9174",
+    storageBucket: "chatbot-d9174.appspot.com",
+    messagingSenderId: "278828509732",
+    appId: "1:278828509732:web:194b1d15fbe1ba079f06b1",
+    measurementId: "G-G9S3H42KS0"
+  };
