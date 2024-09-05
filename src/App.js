368fb0dda30be4ea247a88712456c198314b83dc
Axelito7u7 <galvanflores102@gmail.com>
2024-09-05 11:53:25 -0600

    chatbot

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mi app<code> hecha por</code> Alan connection for fireabse 
         </p>
         <a
           className="App-link"
