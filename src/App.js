2d00639d8fe774a7099b70879fb7ed93c692f903
Axelito7u7 <galvanflores102@gmail.com>
2024-09-04 19:52:45 -0600

    firebaseConectado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
-import '';
+import{
+  useFirebaseApp
+} from'reactfire';
 
 function App() {
+  const firebase = useFirebaseApp();
+  console.log(firebase);
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render((
   <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-    <Suspense>
+    <Suspense fallback={'conetando...'}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
