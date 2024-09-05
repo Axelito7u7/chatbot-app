0ca02c6e4e1ab51c67d6f7eda949581e268f64be
Axelito7u7 <galvanflores102@gmail.com>
2024-09-05 09:42:02 -0600

    stylos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import './App.css';
 import{
   useFirebaseApp
 } from'reactfire';
@@ -11,7 +12,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          preguntas?<code>Cambios firebase</code> por alan
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
