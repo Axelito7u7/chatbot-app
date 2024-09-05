5b254bddec8f63055e6361e2ba307fdd720d623c
Axelito7u7 <galvanflores102@gmail.com>
2024-09-04 19:04:50 -0600

    conexionfire

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebaseconfig from './firebaseConfig'
+import {
+ FirebaseAppProvider
+}from 'reactfire'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render((
+  <FirebaseAppProvider firebaseConfig={firebaseconfig}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </FirebaseAppProvider>
+
+));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
