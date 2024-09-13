dab771f93c90f83f18cc52e7cb2698e3389ea730
Axelito7u7 <galvanflores102@gmail.com>
2024-09-13 16:49:53 -0600

    feat: Añadir componente Header al proyecto

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import logo from './logo.svg';
+import './index.css'; // O './App.css' si es donde lo estás importando
 import './App.css';
-import{
-  useFirebaseApp
-} from'reactfire';
-import HubSpotTracking from './recursos/HubSpotTracking'; // Asegúrate de que la ruta sea correcta
+import './output.css';
+import Example from './componentes/header'; // Importa el header correctamente
 
 function App() {
-  const firebase = useFirebaseApp();
-  console.log(firebase);
-
   return (
-    <div className="App">
-      <HubSpotTracking />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mi app<code> hecha por</code> Alan connection for firebase 
+    <div>
+      <Example /> {/* Componente Header */}
+      <header className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white">
+        <img src="logo.svg" className="App-logo" alt="logo" />
+        <p className="text-2xl">
+          Edita <code>src/App.js</code> y guarda para recargar.
         </p>
         <a
-          className="App-link"
+          className="text-blue-400 hover:text-blue-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
