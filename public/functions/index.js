9316d77c8c2e33a97ae2b4a2a0f47b0b6122ce48
Axelito7u7 <galvanflores102@gmail.com>
2024-09-17 22:44:49 -0600

    feat: Conexion firebase hosting

---
+++ public/functions/index.js
@@ -0,0 +1,26 @@
+const functions = require("firebase-functions");
+const { WebhookClient, Payload } = require("dialogflow-fulfillment");
+
+exports.chatbot = functions.https.onRequest((request, response) => {
+    console.log("req*********:", request.body);
+    console.log("req*********:", request.body);
+    const agent = new WebhookClient({request, response});
+    console.log("DialogFlow Request header:" + JSON.stringify(request.headers));
+    console.log("DialogFlow Request body:" + JSON.stringify(request.body));
+
+    function Welcome(agent){
+        agent.add('Welcome to my agent Firebase Functions');
+    }
+    function fallback(agent){
+        agent.add('I din´n undersand');
+        agent.add('I´m sorry, can you try again?');
+    }
+
+    let intentMap = new Map();
+
+    intentMap.set("Default Welcome Intent", Welcome);
+    intentMap.set("Default Fallback Intent", fallback);
+    agent.handleRequest(intentMap);
+
+
+});
