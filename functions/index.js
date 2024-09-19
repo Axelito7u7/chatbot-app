7a1829e45e568cf46551acc93a0ef8bb0c95e144
Axelito7u7 <galvanflores102@gmail.com>
2024-09-19 10:06:59 -0600

    feact: prueba de Actualizacion de webhook"

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,21 @@
-//const {onRequest} = require("firebase-functions/v2/https");
-//const logger = require("firebase-functions/logger");
+const functions = require('firebase-functions');
+const { WebhookClient } = require('dialogflow-fulfillment');
 
-const functions = require("firebase-functions");
-const { WebhookClient} = require("dialogflow-fulfillment");
+exports.dialogflowFulfillment = functions.https.onRequest((request, response) => {
+    const agent = new WebhookClient({ request, response });
 
-exports.chatbot = functions.https.onRequest((request, response) => {
-    console.log("req*********:", request.body);
-    console.log("req*********:", request.body);
-    const agent = new WebhookClient({request, response});
-    console.log("DialogFlow Request header:" + JSON.stringify(request.headers));
-    console.log("DialogFlow Request body:" + JSON.stringify(request.body));
-
-    function Welcome(agent){
-        agent.add('Welcome to my agent Firebase Functions');
+    function welcomeIntent(agent) {
+        agent.add('Welcome to my Dialogflow agent!');
     }
-    function fallback(agent){
-        agent.add('I din´n undersand');
-        agent.add('I´m sorry, can you try again?');
+
+    function fallbackIntent(agent) {
+        agent.add('I didn’t understand');
+        agent.add('I’m sorry, can you try again?');
     }
 
     let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcomeIntent);
+    intentMap.set('Default Fallback Intent', fallbackIntent);
 
-    intentMap.set("Default Welcome Intent", Welcome);
-    intentMap.set("Default Fallback Intent", fallback);
     agent.handleRequest(intentMap);
-
-
 });
