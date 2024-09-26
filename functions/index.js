fde2625facd817f326d40ad1565bd142c4b44451
Axelito7u7 <galvanflores102@gmail.com>
2024-09-25 18:14:57 -0600

    feact: interaccion para el chatbot

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require("firebase-functions");
-const { WebhookClient} = require("dialogflow-fulfillment");
+const { WebhookClient, Payload } = require("dialogflow-fulfillment");
 const admin = require("firebase-admin");
 
 admin.initializeApp({
@@ -12,24 +12,62 @@ const db = admin.firestore();
 exports.chatbot = functions.https.onRequest((request, response) => {
     const agent = new WebhookClient({ request, response });
 
-
     function MostrarCategorias(agent) {
         const categoriasRef = db.collection("Categorias");
         return categoriasRef
             .get()
             .then((snapshot) => {
-                agent.add("Nuestras categorías: ");
+                if (snapshot.empty) {
+                    agent.add("No hay categorías disponibles.");
+                    return;
+                }
+
+                agent.add("Nuestras categorías:");
+
+                const arrayCategorias = [];
                 snapshot.forEach((doc) => {
-                    agent.add(doc.data().Nombre);
+                    const nombreCategoria = doc.data().Nombre;
+                    const urlCategoria = doc.data().Url; 
+
+                    arrayCategorias.push({
+                        text: nombreCategoria,
+                        link: urlCategoria 
+                    });
                 });
+                const payload = {
+                    richContent: [
+                        [
+                            {
+                                type: "chips",
+                                options: arrayCategorias.map(cat => ({
+                                    text: cat.text,
+                                    link: cat.link 
+                                })),
+                            },
+                        ],
+                    ],
+                };
+
+                // Agregar el payload con los chips
+                agent.add(new Payload(agent.UNSPECIFIED, payload, { rawPayload: true, sendAsMessage: true }));
             })
             .catch(() => {
                 agent.add("Ocurrió un error. Puedes intentar seleccionando otra categoría.");
             });
     }
 
+    function languageHandler(agent) {
+        const language = agent.parameters.language;
+        if (language) {
+          agent.add(`Tu lenguaje es:  ${language}`);
+        } else {
+          agent.add(`Ups mirada`);
+        }
+    }
+
     let intentMap = new Map();
     intentMap.set("MostrarCategorias", MostrarCategorias);
+    intentMap.set("set-language", languageHandler);
 
     agent.handleRequest(intentMap);
 });
